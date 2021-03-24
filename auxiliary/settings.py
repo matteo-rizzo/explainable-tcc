Fixed backprop in temporal attention

auxiliary/settings.py
@@ -3,14 +3,6 @@ import re
 import numpy as np
 import torch
 
-# --- Determinism (for reproducibility) ---
-
-RANDOM_SEED = 0
-
-torch.manual_seed(RANDOM_SEED)
-np.random.seed(RANDOM_SEED)
-torch.backends.cudnn.benchmark = False
-
 # --- Device (cpu or cuda:n) ---
 
 DEVICE_TYPE = "cuda:0"
@@ -33,6 +25,15 @@ def get_device() -> torch.device:
 
 
 DEVICE = get_device()
+
+
+# --- Determinism (for reproducibility) ---
+
+def make_deterministic(random_seed: int = 0):
+    torch.manual_seed(random_seed)
+    np.random.seed(random_seed)
+    torch.backends.cudnn.benchmark = False
+
 
 # The ids of the sequences to be monitored at training time (e.g., ["0", "1", "2"])
 TEST_VIS_IMG = []

classes/modules/attention/TemporalAttentionModule.py
@@ -1,11 +1,5 @@
-from typing import List
-
-import numpy as np
 import torch
-import torch.nn.functional as F
 from torch import nn, Tensor
-
-from auxiliary.settings import DEVICE
 
 
 class TemporalAttentionModule(nn.Module):
@@ -14,8 +8,9 @@ class TemporalAttentionModule(nn.Module):
         super().__init__()
         self.phi_x = nn.Linear(features_size, 1, bias=False)
         self.phi_h = nn.Linear(hidden_size, 1, bias=False)
+        self.softmax = nn.Softmax(dim=0)
 
-    def __generate_energy(self, masked_features: Tensor, hidden: Tensor) -> np.ndarray:
+    def __generate_energy(self, masked_features: Tensor, hidden: Tensor) -> Tensor:
         """
         Computes energy as e_ti = phi(H_t-1, masked_X_i) = phi(H_t-1) + phi(masked_X_i)
         @param masked_features: the i-th masked spatial features map
@@ -25,30 +20,20 @@ class TemporalAttentionModule(nn.Module):
         att_x = self.phi_x(torch.mean(torch.mean(masked_features, dim=2), dim=1))
         att_h = self.phi_h(torch.mean(torch.mean(hidden, dim=3), dim=2))
         e = att_x + att_h
-        return e.squeeze(dim=1).cpu().data.numpy()
-
-    @staticmethod
-    def __energies_to_weights(energies: List) -> torch.Tensor:
-        """
-        Applies softmax to energies to scale them in [0, 1]
-        @param energies: a list of energies e_ti (of length time steps), one for each attended frame
-        @return: the weights w_ti = exp(e_ti)/sum_i^n(exp(e_ti))
-        """
-        weights = F.softmax(torch.from_numpy(np.asarray(energies).squeeze()), dim=0).to(DEVICE)
-        return weights.unsqueeze(1).unsqueeze(2).unsqueeze(3)
+        return e
 
     def forward(self, x: Tensor, h: Tensor) -> Tensor:
         """
-        @param x: the sequences of frames of shape "bs x ts x nc x h x w"
+        @param x: the sequences of frames of shape "ts x nc x h x w"
         @param h: the hidden state of an RNN
         @return: the normalized illuminant prediction
         """
-        time_steps = x.shape[0]
+        ts = x.shape[0]
 
         # Compute energies as e_ti = phi(H_t-1, masked_X_i)
-        energies = [self.__generate_energy(x[i, :, :, :], h) for i in range(time_steps)]
+        energies = torch.cat([self.__generate_energy(x[i, :, :, :], h) for i in range(ts)], dim=0)
 
         # Energies to temporal weights via softmax: w_ti = exp(e_ti)/sum_i^n(exp(e_ti))
-        weights = self.__energies_to_weights(energies)
+        weights = self.softmax(energies).unsqueeze(1).unsqueeze(2)
 
         return weights

classes/modules/multiframe/att_tccnet/AttTCCNet.py
@@ -4,8 +4,8 @@ import torch
 from torch import nn
 from torch.nn.functional import normalize
 
-from classes.modules.attention.spatial_attention import SpatialAttentionModule
-from classes.modules.attention.temporal_attention import TemporalAttentionModule
+from classes.modules.attention.SpatialAttentionModule import SpatialAttentionModule
+from classes.modules.attention.TemporalAttentionModule import TemporalAttentionModule
 from classes.modules.common.BaseTCCNet import BaseTCCNet
 from classes.modules.common.squeezenet.SqueezeNetLoader import SqueezeNetLoader
 
@@ -47,7 +47,7 @@ class AttTCCNet(BaseTCCNet):
         hidden, cell = self.init_hidden(batch_size, h, w)
 
         hidden_states, temp_mask = [], []
-        for _ in range(time_steps):
+        for t in range(time_steps):
             # Temporal attention
             temp_weights = self.temp_att(spat_weighted_est, hidden)
             spat_temp_weighted_est = torch.div(torch.sum(spat_weighted_est * temp_weights, dim=0), time_steps)

classes/modules/multiframe/conf_att_tccnet/ConfAttTCCNet.py
@@ -4,7 +4,7 @@ import torch
 from torch.nn.functional import normalize
 
 from auxiliary.utils import scale
-from classes.modules.attention.temporal_attention import TemporalAttentionModule
+from classes.modules.attention.TemporalAttentionModule import TemporalAttentionModule
 from classes.modules.common.BaseTCCNet import BaseTCCNet
 from classes.modules.common.ConfidenceFCN import ConfidenceFCN
 

classes/modules/multiframe/conf_tccnet/ConfTCCNet.py
@@ -15,18 +15,20 @@ class ConfTCCNet(BaseTCCNet):
 
     def __init__(self, hidden_size: int = 128, kernel_size: int = 5):
         super().__init__(rnn_input_size=3, hidden_size=hidden_size, kernel_size=kernel_size)
+
+        # Confidences as spatial and temporal attention
         self.fcn = ConfidenceFCN()
 
     def forward(self, x: torch.Tensor) -> Tuple:
         batch_size, time_steps, num_channels, h, w = x.shape
         x = x.view(batch_size * time_steps, num_channels, h, w)
 
-        # Spatial confidence
+        # Spatial confidence (confidence mask)
         rgb, spat_confidence = self.fcn(x)
         spat_weighted_est = scale(rgb * spat_confidence).clone()
 
-        # Temporal confidence
-        temp_confidence = F.softmax(torch.mean(torch.mean(spat_confidence.squeeze(1), dim=1), dim=1))
+        # Temporal confidence (average of confidence mask)
+        temp_confidence = F.softmax(torch.mean(torch.mean(spat_confidence.squeeze(1), dim=1), dim=1), dim=0)
         spat_temp_weighted_est = spat_weighted_est * temp_confidence.unsqueeze(1).unsqueeze(2).unsqueeze(3)
 
         _, _, h, w = spat_weighted_est.shape

train/tcc/train_interpretable_tccnet.py
@@ -1,10 +1,11 @@
+import argparse
 import os
 import time
 
 import torch.utils.data
 from torch.utils.data import DataLoader
 
-from auxiliary.settings import DEVICE
+from auxiliary.settings import DEVICE, make_deterministic
 from auxiliary.utils import log_experiment, log_metrics, print_metrics, log_time
 from classes.data.datasets.TemporalColorConstancy import TemporalColorConstancy
 from classes.modules.multiframe.att_tccnet.ModelAttTCCNet import ModelAttTCCNet
@@ -16,8 +17,8 @@ from classes.training.LossTracker import LossTracker
 MODEL_TYPE = "conf_tccnet"
 DATA_FOLDER = "tcc_split"
 EPOCHS = 2000
-BATCH_SIZE = 1
 LEARNING_RATE = 0.00003
+RANDOM_SEED = 0
 
 RELOAD_CHECKPOINT = False
 PATH_TO_PTH_CHECKPOINT = os.path.join("trained_models", "{}_{}".format(MODEL_TYPE, DATA_FOLDER), "model.pth")
@@ -25,29 +26,33 @@ PATH_TO_PTH_CHECKPOINT = os.path.join("trained_models", "{}_{}".format(MODEL_TYP
 MODELS = {"att_tccnet": ModelAttTCCNet, "conf_tccnet": ModelConfTCCNet, "conf_att_tccnet": ModelConfAttTCCNet}
 
 
-def main():
+def main(opt):
+    model_type = opt.model_type
+    data_folder = opt.data_folder
+    epochs = opt.epochs
+    learning_rate = opt.lr
     evaluator = Evaluator()
 
-    path_to_log = os.path.join("train", "tcc", "logs", "", "{}_{}_{}".format(MODEL_TYPE, DATA_FOLDER, + time.time()))
+    path_to_log = os.path.join("train", "tcc", "logs", "", "{}_{}_{}".format(model_type, data_folder, + time.time()))
     os.makedirs(path_to_log)
 
     path_to_metrics_log = os.path.join(path_to_log, "metrics.csv")
     path_to_experiment_log = os.path.join(path_to_log, "experiment.json")
-    log_experiment(MODEL_TYPE, DATA_FOLDER, LEARNING_RATE, path_to_experiment_log)
+    log_experiment(model_type, data_folder, learning_rate, path_to_experiment_log)
 
-    print("\nLoading data from '{}':".format(DATA_FOLDER))
+    print("\nLoading data from '{}':".format(data_folder))
 
-    training_set = TemporalColorConstancy(mode="train", data_folder=DATA_FOLDER)
-    train_loader = DataLoader(dataset=training_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=8)
+    training_set = TemporalColorConstancy(mode="train", data_folder=data_folder)
+    train_loader = DataLoader(dataset=training_set, batch_size=1, shuffle=True, num_workers=8)
 
-    test_set = TemporalColorConstancy(mode="test", data_folder=DATA_FOLDER)
-    test_loader = DataLoader(dataset=test_set, batch_size=BATCH_SIZE, num_workers=8)
+    test_set = TemporalColorConstancy(mode="test", data_folder=data_folder)
+    test_loader = DataLoader(dataset=test_set, batch_size=1, num_workers=8)
 
     training_set_size, test_set_size = len(training_set), len(test_set)
     print("Training set size: ... {}".format(training_set_size))
     print("Test set size: ....... {}\n".format(test_set_size))
 
-    model = MODELS[MODEL_TYPE]()
+    model = MODELS[model_type]()
 
     if RELOAD_CHECKPOINT:
         print('\n Reloading checkpoint - pretrained model stored at: {} \n'.format(PATH_TO_PTH_CHECKPOINT))
@@ -55,15 +60,15 @@ def main():
 
     model.print_network()
     model.log_network(path_to_log)
-    model.set_optimizer(learning_rate=LEARNING_RATE)
+    model.set_optimizer(learning_rate)
 
     best_val_loss, best_metrics = 100.0, evaluator.get_best_metrics()
     train_loss, val_loss = LossTracker(), LossTracker()
 
-    for epoch in range(EPOCHS):
+    for epoch in range(epochs):
 
         print("\n--------------------------------------------------------------")
-        print("\t\t Training epoch {}/{}".format(epoch + 1, EPOCHS))
+        print("\t\t Training epoch {}/{}".format(epoch + 1, epochs))
         print("--------------------------------------------------------------\n")
 
         model.train_mode()
@@ -77,7 +82,7 @@ def main():
 
             if i % 5 == 0:
                 print("[ Epoch: {}/{} - Batch: {}/{} ] | [ Train loss: {:.4f} ]"
-                      .format(epoch + 1, EPOCHS, i + 1, training_set_size, loss))
+                      .format(epoch + 1, epochs, i + 1, training_set_size, loss))
 
         train_time = time.time() - start
         log_time(time=train_time, time_type="train", path_to_log=path_to_experiment_log)
@@ -134,4 +139,20 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_type", type=str, default=MODEL_TYPE)
+    parser.add_argument('--data_folder', type=str, default=DATA_FOLDER)
+    parser.add_argument('--epochs', type=int, default=EPOCHS)
+    parser.add_argument('--lr', type=float, default=LEARNING_RATE)
+    parser.add_argument('--random_seed', type=int, default=RANDOM_SEED)
+    opt = parser.parse_args()
+
+    print("\n *** Training configuration ***")
+    print("\t Model type ...... : {}".format(opt.model_type))
+    print("\t Data folder ..... : {}".format(opt.data_folder))
+    print("\t Epochs .......... : {}".format(opt.epochs))
+    print("\t Learning rate ... : {}".format(opt.lr))
+    print("\t Random seed ..... : {}".format(opt.random_seed))
+
+    make_deterministic(opt.random_seed)
+    main(opt)